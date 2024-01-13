3deca811bbe5a88512c3a9b3dca96b8d872d42c8
Louis Peace <chieflopesekede@gmail.com>
2024-01-13 16:48:15 +0100

    Added TailwindCSS and some other edits

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css'
+import "./App.css";
+import LandingPage from "./Pages/LandingPage";
 
 function App() {
-
   return (
     <>
-    
+      <div className="flex">
+        <LandingPage />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/Home/Nav.jsx
@@ -0,0 +1,16 @@
+const Nav = () => {
+  return (
+    <nav className="flex items-center justify-center">
+      <div>Logo</div>
+      <ul className="flex ml-9 md:ml-[30rem]">
+        <li>Home</li>
+        <li>About</li>
+        <li>Menu</li>
+        <li>Contact</li>
+      </ul>
+      <div>Sign Up</div>
+    </nav>
+  );
+};
+
+export default Nav;
