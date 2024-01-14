d2b093d73d664183e46b7a265b40157059d63a42
Louis Peace <chieflopesekede@gmail.com>
2024-01-14 15:33:12 +0100

    Added background to the nav

--- src/Components/Home/Nav.jsx
+++ src/Components/Home/Nav.jsx
@@ -1,18 +1,18 @@
 import Logo from "../../assets/Logo/LOGO-OF-CHEESECAKE.png";
 
-
 const Nav = () => {
   return (
-    <nav className="flex items-center justify-center font-inika">
+    <nav className="flex items-center justify-center font-inika ">
       <div className="md:ml-[-11.5rem]">
         <img src={Logo} alt="logo" />
       </div>
-      <ul className="flex md:ml-[20rem] space-x-7">
+      <ul className="flex md:ml-[17rem] space-x-7 bg-[#6F4436CC] md:w-[20rem] h-10 justify-center items-center rounded-xl text-white font-bold">
         <li>Home</li>
         <li>About</li>
         <li>Menu</li>
         <li>Contact</li>
       </ul>
+
       <div className="md:ml-[17rem]">Sign Up</div>
     </nav>
   );
