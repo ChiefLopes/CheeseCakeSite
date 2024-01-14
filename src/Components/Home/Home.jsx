865bee4466679bf36feb9a4a1f90eb390a7382d3
Louis Peace <chieflopesekede@gmail.com>
2024-01-14 09:30:50 +0100

    added the cheesecake logo to the home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,30 +1,36 @@
 import Nav from "./Nav";
+import Cheesecake from "../../assets/bgphoto/Cheesecake.png"
+
+// imported Icon 
+import { FaCirclePlay } from "react-icons/fa6";
 
 const Home = () => {
   return (
-    <div className="bg-cover bg-center h-screen background font-inika ">
+    <div className="bg-cover bg-center h-screen background font-inika">
       <Nav />
 
-      <div>
-        <div className="text-[#523728] space-y-3">
+      <div className="flex ">
+        <div className="text-[#523728] space-y-5 mt-14 ml-20">
           <p className=" text-5xl tracking-wide leading-[4rem] ">
             Bite The World of <br /> Cheesecake Wonders
           </p>
-          <p>
+          <p className="text-lg">
             We always make our customer happy by providing <br /> as many
             choices as possible
           </p>
 
-          <div className="">
-            <button className="bg-[#6A4028] text-white rounded-2xl h-9 w-28">
+          <div className="space-x-4 pt-3 flex flex-row font-bold">
+            <button className="bg-[#6A4028] text-white rounded-3xl h-12 w-40 text-center">
               Get Started
-            </button>
-            <button className="border border-[#6A4028] rounded-2xl h-9 w-28">
-              Watch Demo
+            </button> 
+            <button className="border border-[#6A4028] rounded-3xl h-12 w-40 flex items-center justify-center space-x-2">
+              <FaCirclePlay /> <p>Watch demo</p>
             </button>
           </div>
         </div>
-        <div></div>
+              <div>
+                  <img src={Cheesecake} alt="" />
+        </div>
       </div>
     </div>
   );
--- src/Components/Home/Nav.jsx
+++ src/Components/Home/Nav.jsx
@@ -4,7 +4,7 @@ import Logo from "../../assets/Logo/LOGO-OF-CHEESECAKE.png";
 const Nav = () => {
   return (
     <nav className="flex items-center justify-center font-inika">
-      <div className="md:ml-[-9rem]">
+      <div className="md:ml-[-11.5rem]">
         <img src={Logo} alt="logo" />
       </div>
       <ul className="flex md:ml-[20rem] space-x-7">
