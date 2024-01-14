67ec6db1492c7d639857eebc436b39cec5d27086
Louis Peace <chieflopesekede@gmail.com>
2024-01-14 11:45:00 +0100

    starting with the AboutUs Page

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -1,10 +1,31 @@
+import About from "../assets/PgImgs/About.png"
+
 const AboutUs = () => {
   return (
-    <div className=" h-screen">
-      <div>
-        <img src="" alt="" />
+    <div className="flex ml-20 space-x-9">
+      <div className="h-20  md:w-[30rem]">
+        <img src={About} alt="" />
       </div>
-      <div></div>
+      <div className="flex flex-col align-center">
+        <span>
+          <p>We Love Cheesecake</p>
+          <p>Discover Our Cheesecake Story</p>
+        </span>
+        <span>
+          <p>
+            At Cheesecake Love, quality is our cornerstone. Each <br />{" "}
+            cheesecake is crafted with the finest ingredients, <br /> meticulous
+            attention to detail, and a dash of creativity.
+          </p>
+          <p>
+            We believe in delivering not just desserts but moments of <br />{" "}
+            sheer indulgence.
+          </p>
+        </span>
+        <button className="bg-[#6A4028] text-white rounded-3xl h-12 w-40 text-center">
+          Read More
+        </button>
+      </div>
     </div>
   );
 };
