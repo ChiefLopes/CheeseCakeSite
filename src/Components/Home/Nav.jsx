03d4758643166cf935cf1db0d3e43e9ab5e58c91
Louis Peace <chieflopesekede@gmail.com>
2024-01-14 07:42:48 +0100

    added the logo to its folder

--- src/Components/Home/Nav.jsx
+++ src/Components/Home/Nav.jsx
@@ -1,8 +1,11 @@
+import Logo from "../../assets/Logo/LOGO-OF-CHEESECAKE.png";
+
+
 const Nav = () => {
   return (
     <nav className="flex items-center justify-center p-8 font-inika ">
           <div className="md:ml-[-7rem]">
-              <img src="" alt="logo" />
+              <img src={Logo} alt="logo" />
       </div>
       <ul className="flex md:ml-[20rem] space-x-7">
         <li>Home</li>
