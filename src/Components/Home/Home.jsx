b4f8ea68cc052251f41053fbc0f650a3c49442dc
Louis Peace <chieflopesekede@gmail.com>
2024-01-14 08:08:48 +0100

    added the logo to the nav also added color to the text @ home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,13 +2,12 @@ import Nav from "./Nav";
 
 const Home = () => {
   return (
-    <div
-      className="bg-cover bg-center h-screen background">
+    <div className="bg-cover bg-center h-screen background font-inika ">
       <Nav />
 
       <div>
         <div>
-          <p>Bite The World of Cheesecake Wonders</p>
+          <p className="text-[#523728] text-5xl tracking-wide leading-12">Bite The World of <br /> Cheesecake Wonders</p>
           <p>
             We always make our customer happy by providing as many choices as
             possible
--- src/Components/Home/Nav.jsx
+++ src/Components/Home/Nav.jsx
@@ -3,9 +3,9 @@ import Logo from "../../assets/Logo/LOGO-OF-CHEESECAKE.png";
 
 const Nav = () => {
   return (
-    <nav className="flex items-center justify-center p-8 font-inika ">
-          <div className="md:ml-[-7rem]">
-              <img src={Logo} alt="logo" />
+    <nav className="flex items-center justify-center font-inika">
+      <div className="md:ml-[-9rem]">
+        <img src={Logo} alt="logo" />
       </div>
       <ul className="flex md:ml-[20rem] space-x-7">
         <li>Home</li>
