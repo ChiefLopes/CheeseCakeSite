16c7c863583580289d65cf2d31d8bf14c9fc0154
Louis Peace <chieflopesekede@gmail.com>
2024-01-14 10:39:18 +0100

    added the cheesecake logo to the home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import Nav from "./Nav";
-import Cheesecake from "../../assets/bgphoto/Cheesecake.png"
+import Cheesecake from "../../assets/bgphoto/Cheesecake.png";
 
-// imported Icon 
+// imported Icon
 import { FaCirclePlay } from "react-icons/fa6";
 
 const Home = () => {
@@ -9,7 +9,7 @@ const Home = () => {
     <div className="bg-cover bg-center h-screen background font-inika">
       <Nav />
 
-      <div className="flex ">
+      <div className="flex mx-auto">
         <div className="text-[#523728] space-y-5 mt-14 ml-20">
           <p className=" text-5xl tracking-wide leading-[4rem] ">
             Bite The World of <br /> Cheesecake Wonders
@@ -22,14 +22,14 @@ const Home = () => {
           <div className="space-x-4 pt-3 flex flex-row font-bold">
             <button className="bg-[#6A4028] text-white rounded-3xl h-12 w-40 text-center">
               Get Started
-            </button> 
+            </button>
             <button className="border border-[#6A4028] rounded-3xl h-12 w-40 flex items-center justify-center space-x-2">
               <FaCirclePlay /> <p>Watch demo</p>
             </button>
           </div>
         </div>
-              <div>
-                  <img src={Cheesecake} alt="" />
+        <div className="ml-20 pb-12">
+          <img src={Cheesecake} alt="" />
         </div>
       </div>
     </div>
