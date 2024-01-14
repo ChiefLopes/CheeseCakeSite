e87b41a185f6fad0aec8a2421f5e898f19c012b9
Louis Peace <chieflopesekede@gmail.com>
2024-01-14 07:09:27 +0100

    Added the Background to the Home page

--- src/Components/Home/Nav.jsx
+++ src/Components/Home/Nav.jsx
@@ -1,7 +1,7 @@
 const Nav = () => {
   return (
     <nav className="flex items-center justify-center p-8 font-inika ">
-      <div className="m-4">Logo</div>
+      <div className="md:ml-[-7rem]">Logo</div>
       <ul className="flex md:ml-[20rem] space-x-7">
         <li>Home</li>
         <li>About</li>
