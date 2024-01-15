8925f8c229f762d4e4e49edf203b77cb867e21f0
Louis Peace <chieflopesekede@gmail.com>
2024-01-15 08:18:20 +0100

    Added the react-router-dom for easily routing, also added the Home and AboutUs pages to the Route

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,11 +1,13 @@
 import Home from "../Components/Home/Home";
 import AboutUs from "./AboutUs";
 
+
 const LandingPage = () => {
+  
   return (
     <>
       <Home />
-      <AboutUs />
+          <AboutUs />
     </>
   );
 };
