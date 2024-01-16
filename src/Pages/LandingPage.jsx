755ac4f69b327181ae96f87e17eda1519d459135
Ediana-Ekanem <edianaekanem@gmail.com>
2024-01-16 15:52:05 +0100

    updating blog section

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,11 +1,13 @@
 import AboutUs from "../Components/AboutUs";
+import Blog from "../Components/Blog";
 import Home from "../Components/Home/Home";
 
 const LandingPage = () => {
   return (
     <div>
-          <Home />
-          <AboutUs />
+      <Home />
+      <AboutUs />
+      <Blog />
     </div>
   );
 };
