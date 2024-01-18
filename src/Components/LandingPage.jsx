f237e57aa4f19a44930385b946578cf4234013ba
Louis Peace <chieflopesekede@gmail.com>
2024-01-18 07:09:35 +0100

    menu

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,13 +1,18 @@
 import AboutUs from "../Components/AboutUs";
 import Blog from "../Components/Blog";
 import Home from "../Components/Home/Home";
+<<<<<<< HEAD:src/Components/LandingPage.jsx
 import AboutUs from "./AboutUs";
+=======
+import Menu from "../Components/Menu";
+>>>>>>> 1d6107b (updating the menu section):src/Pages/LandingPage.jsx
 
 const LandingPage = () => {
   return (
     <div>
       <Home />
       <AboutUs />
+      <Menu />
       <Blog />
     </div>
   );
